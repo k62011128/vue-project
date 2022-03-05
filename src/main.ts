34ffd927de66639ad968a1dcf8c91cdd21b3c054
kanami <jdzhuzhuoning@163.com>
2022-03-05 13:05:44 +0800

    2022.3.4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import { Button, Select } from 'element-ui';
 import './assets/styles/index.scss'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Button);
+Vue.use(Select);
 createApp(App).mount('#app')
